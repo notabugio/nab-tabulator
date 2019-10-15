ce431ae8deeff17120c29a53fb1417cf9a8ac42d
go1dfish <me@go1dfish.me>
2019-10-15 15:54:28 +0000

    Stability improvements

--- src/NabTabulator.ts
+++ src/NabTabulator.ts
@@ -1,119 +1,129 @@
-import { ChainGunSear, GunGraph, GunProcessQueue } from "@notabug/chaingun-sear"
-import SocketClusterGraphConnector from "@notabug/chaingun-socket-cluster-connector"
-import { Query, Config } from "@notabug/peer"
-import { pubFromSoul, unpackNode } from "@notabug/gun-sear"
-import { idsToTabulate, tabulateThing } from "./functions"
-import LmdbGraphConnector from "@notabug/chaingun-lmdb"
+import LmdbGraphConnector from '@notabug/chaingun-lmdb';
+import {
+  ChainGunSear,
+  GunGraph,
+  GunProcessQueue
+} from '@notabug/chaingun-sear';
+import SocketClusterGraphConnector from '@notabug/chaingun-socket-cluster-connector';
+import { pubFromSoul, unpackNode } from '@notabug/gun-sear';
+import { Config, Query } from '@notabug/peer';
+import { idsToTabulate, tabulateThing } from './functions';
 
 interface Opts {
-  socketCluster: any
-  lmdb?: any
+  readonly socketCluster: any;
+  readonly lmdb?: any;
 }
 
 const DEFAULT_OPTS: Opts = {
-  socketCluster: {
-    hostname: process.env.GUN_SC_HOST || "127.0.0.1",
-    port: process.env.GUN_SC_PORT || 4444,
-    path: process.env.GUN_SC_PATH || "/socketcluster",
-    autoReconnect: true,
-    autoReconnectOptions: {
-      initialDelay: 1,
-      randomness: 100,
-      maxDelay: 500
-    }
-  },
   lmdb: {
-    path: process.env.LMDB_PATH || "./data",
-    mapSize: 1024 ** 4
+    mapSize: 1024 ** 4,
+    path: process.env.LMDB_PATH || './data'
+  },
+  socketCluster: {
+    autoReconnect: true,
+    hostname: process.env.GUN_SC_HOST || '127.0.0.1',
+    path: process.env.GUN_SC_PATH || '/socketcluster',
+    port: process.env.GUN_SC_PORT || 4444
   }
-}
+};
 
 Config.update({
   indexer: process.env.GUN_SC_PUB,
   tabulator: process.env.NAB_TABULATOR || process.env.GUN_SC_PUB
-})
+});
 
 export class NabTabulator extends ChainGunSear {
-  socket: SocketClusterGraphConnector
-  lmdb: LmdbGraphConnector
-  tabulatorQueue: GunProcessQueue
-
-  gun: ChainGunSear // temp compatibility thing for notabug-peer transition
+  protected readonly socket: SocketClusterGraphConnector;
+  protected readonly lmdb: LmdbGraphConnector;
+  protected readonly tabulatorQueue: GunProcessQueue;
 
   constructor(options = DEFAULT_OPTS) {
     const { socketCluster: scOpts, lmdb: lmdbOpts, ...opts } = {
       ...DEFAULT_OPTS,
       ...options
-    }
+    };
 
-    const graph = new GunGraph()
-    const lmdb = new LmdbGraphConnector(lmdbOpts)
-    lmdb.sendRequestsFromGraph(graph as any)
-    const socket = new SocketClusterGraphConnector(options.socketCluster)
-    socket.sendPutsFromGraph(graph as any)
-    graph.connect(lmdb as any)
+    const graph = new GunGraph();
+    const lmdb = new LmdbGraphConnector(lmdbOpts);
+    lmdb.sendRequestsFromGraph(graph as any);
+    const socket = new SocketClusterGraphConnector(options.socketCluster);
+    socket.sendPutsFromGraph(graph as any);
+    graph.connect(lmdb as any);
 
-    super({ graph, ...opts })
-    this.gun = this
-    this.lmdb = lmdb
-    this.directRead = this.directRead.bind(this)
+    super({ graph, ...opts });
+    socket.socket.on('connect', this.authenticate.bind(this));
+    this.lmdb = lmdb;
+    this.directRead = this.directRead.bind(this);
 
-    this.tabulatorQueue = new GunProcessQueue()
-    this.tabulatorQueue.middleware.use(id => tabulateThing(this, id))
+    this.tabulatorQueue = new GunProcessQueue();
+    this.tabulatorQueue.middleware.use(id => tabulateThing(this, id));
 
-    this.socket = socket
-    this.authenticateAndListen()
+    this.socket = socket;
+    this.socket.subscribeToChannel(
+      'gun/put/diff',
+      this.didReceiveDiff.bind(this),
+      { waitForAuth: true }
+    );
+    this.authenticate();
+
+    setInterval(() => this.authenticate(), 1000 * 60 * 30);
   }
 
-  newScope(): any {
-    return Query.createScope(this, {
-      getter: this.directRead,
-      unsub: true
-    })
+  public newScope(): any {
+    return Query.createScope(
+      { gun: this },
+      {
+        getter: this.directRead,
+        unsub: true
+      }
+    );
   }
 
-  directRead(soul: string) {
-    return new Promise(ok => {
-      this.lmdb.get({
-        soul,
-        cb: (msg: any) => {
-          const node = (msg && msg.put && msg.put[soul]) || undefined
-          if (pubFromSoul(soul)) unpackNode(node, "mutable")
-
-          ok(node)
-        }
-      })
-    })
-  }
-
-  didReceiveDiff(msg: any) {
-    const ids = idsToTabulate(msg)
-    if (ids.length) {
-      this.tabulatorQueue.enqueueMany(ids)
-    }
-    this.tabulatorQueue.process()
-  }
-
-  authenticateAndListen() {
+  public authenticate(): void {
     if (process.env.GUN_SC_PUB && process.env.GUN_SC_PRIV) {
       this.socket
         .authenticate(process.env.GUN_SC_PUB, process.env.GUN_SC_PRIV)
         .then(() => {
-          console.log(`Logged in as ${process.env.GUN_SC_PUB}`)
+          // tslint:disable-next-line: no-console
+          console.log(`Logged in as ${process.env.GUN_SC_PUB}`);
+          // tslint:disable-next-line: no-console
+          console.log('socket id', this.socket.socket.id);
         })
-        .catch(err => console.error("Error logging in:", err.stack || err))
+        // tslint:disable-next-line: no-console
+        .catch(err => console.error('Error logging in:', err.stack || err));
     }
 
     if (process.env.GUN_ALIAS && process.env.GUN_PASSWORD) {
       this.user()
         .auth(process.env.GUN_ALIAS, process.env.GUN_PASSWORD)
         .then(() => {
-          console.log(`Logged in as ${process.env.GUN_ALIAS}`)
-          this.socket.subscribeToChannel(
-            "gun/put/diff",
-            this.didReceiveDiff.bind(this)
-          )
-        })
+          // tslint:disable-next-line: no-console
+          console.log(`Logged in as ${process.env.GUN_ALIAS}`);
+        });
     }
+  }
+
+  protected directRead(soul: string): Promise<any> {
+    return new Promise(ok => {
+      this.lmdb.get({
+        cb: (msg: any) => {
+          const node = (msg && msg.put && msg.put[soul]) || undefined;
+          if (pubFromSoul(soul)) {
+            unpackNode(node, 'mutable');
+          }
+
+          ok(node);
+        },
+        soul
+      });
+    });
+  }
+
+  protected didReceiveDiff(msg: any): void {
+    const ids = idsToTabulate(msg);
+    if (ids.length) {
+      this.tabulatorQueue.enqueueMany(ids);
+    }
+    this.tabulatorQueue.process();
   }
 }
