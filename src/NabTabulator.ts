36bcd6e56df2f0ec7e6cf44f3e0017d0de67d7ca
go1dfish <me@go1dfish.me>
2019-12-25 11:13:19 -0800

    Support federation adapter

--- src/NabTabulator.ts
+++ src/NabTabulator.ts
@@ -1,5 +1,13 @@
 import createAdapter from '@chaingun/node-adapters'
-import { Config, GunProcessQueue, NotabugClient } from '@notabug/client'
+import {
+  Config,
+  createFederationAdapter,
+  GunGraphAdapter,
+  GunProcessQueue,
+  NotabugClient
+} from '@notabug/client'
+import fs from 'fs'
+import yaml from 'js-yaml'
 import { tabulateThing } from './functions'
 import { processDiff } from './tabulator'
 
@@ -8,13 +16,36 @@ Config.update({
   tabulator: process.env.NAB_TABULATOR || process.env.GUN_SC_PUB
 })
 
+const PEERS_CONFIG_FILE = './peers.yaml'
+
+let peersConfigTxt = ''
+
+try {
+  peersConfigTxt = fs.readFileSync(PEERS_CONFIG_FILE).toString()
+} catch (e) {
+  // tslint:disable-next-line: no-console
+  console.warn('Peers config missing', PEERS_CONFIG_FILE, e.stack)
+}
+
+const peerUrls = yaml.safeLoad(peersConfigTxt) || []
+
 export class NabTabulator extends NotabugClient {
+  public readonly internalAdapter: GunGraphAdapter
   protected readonly tabulatorQueue: GunProcessQueue<any>
   protected readonly diffTabulatorQueue: GunProcessQueue<any>
 
   constructor(...opts) {
-    const dbAdapter = createAdapter()
+    const internalAdapter = createAdapter()
+    const dbAdapter = createFederationAdapter(
+      internalAdapter,
+      peerUrls,
+      internalAdapter,
+      {
+        putToPeers: true
+      }
+    )
     super(dbAdapter, ...opts)
+    this.internalAdapter = internalAdapter
     this.socket.socket.on('connect', this.authenticate.bind(this))
     this.tabulatorQueue = new GunProcessQueue()
     this.tabulatorQueue.middleware.use(id => tabulateThing(this, id))
@@ -27,7 +58,11 @@ export class NabTabulator extends NotabugClient {
     )
   }
 
-  protected didReceiveDiff(msg: any): void {
+  protected async didReceiveDiff(msg: any): Promise<void> {
+    if (msg.put && !(await this.internalAdapter.put(msg.put))) {
+      return // Avoid re-processing own writes
+    }
+
     if (msg && msg.put) {
       this.diffTabulatorQueue.enqueue(msg.put)
       this.diffTabulatorQueue.process()
